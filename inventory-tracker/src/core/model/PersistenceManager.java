ade7145fd680b58a8e2973bc3bf84803186936cb
Aaron Miller <l3370x@gmail.com>
2013-07-02 17:15:46 -0600

    added javadoc stuff

--- inventory-tracker/src/core/model/PersistenceManager.java
+++ inventory-tracker/src/core/model/PersistenceManager.java
@@ -29,7 +29,7 @@ public enum PersistenceManager {
 	 * @throws HITException if saving the data failed for some reason.
 	 */
 	public void save() throws HITException{
-		
+		StorageUnitManager.values();
     }
 	
 	/**
