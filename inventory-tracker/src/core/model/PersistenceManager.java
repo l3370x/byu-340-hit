185c07e4446e3e58d4757f859259304cbb4ca822
Aaron Miller <l3370x@gmail.com>
2013-07-08 22:51:04 -0600

    implemented PersistenceManager

--- inventory-tracker/src/core/model/PersistenceManager.java
+++ inventory-tracker/src/core/model/PersistenceManager.java
@@ -3,8 +3,12 @@
  */
 package core.model;
 
-import core.model.exception.HITException;
 
+import core.model.exception.HITException;
+import core.model.exception.Severity;
+import core.model.InventoryManager;
+import java.util.*;
+import java.io.*;
 
 /**
  * The {@code PersistenceManager} will be used to handle loading and saving
@@ -27,9 +31,23 @@ public enum PersistenceManager {
 	 * @throws HITException if saving the data failed for some reason.
 	 */
 	public void save() throws HITException {
-		
-    }
-	
+		Iterator<StorageUnit> storageUnits = InventoryManager.Factory.getInventoryManager().getContents().iterator();
+
+		try {
+			FileOutputStream fileOut = new FileOutputStream("save.data");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			while ( storageUnits.hasNext() ) {
+				out.writeObject(storageUnits.next());
+			}
+			out.close();
+			fileOut.close();
+		} catch(Exception e) {
+			throw new HITException(Severity.WARNING,e.getMessage());
+		}
+
+
+	}
+
 	/**
 	 * Load the data that the program will use.  If there is no data that was 
 	 * read in, do nothing.  This should only be called at the start of the 
@@ -41,16 +59,42 @@ public enum PersistenceManager {
 	 * @throws HITException if there was an error in reading the data.
 	 */
 	public void load() throws HITException {
-		
+		InventoryManager i =  InventoryManager.Factory.getInventoryManager();
+		ArrayList<StorageUnit> storageUnits = new ArrayList<StorageUnit>();
+		try {
+			FileInputStream fileIn = new FileInputStream("save.data");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			try {
+				while(true) {
+					storageUnits.add((StorageUnit) in.readObject());
+				}
+			} catch (OptionalDataException e) {
+		        if (!e.eof) throw e;
+		    } finally {
+		    	System.out.println(storageUnits.size());
+		        in.close();
+		    }
+			System.out.println(storageUnits.size());
+			in.close();
+			fileIn.close();
+		} catch(Exception e) {
+			if(!e.getClass().equals(EOFException.class)){
+				throw new HITException(Severity.WARNING,e.getMessage());
+			}
+		}
+		Iterator<StorageUnit> storageUnitIterator = storageUnits.iterator();
+		while(storageUnitIterator.hasNext()){
+			i.add(storageUnitIterator.next());
+		}
 	}
-	
+
 	/**
 	 * Update the saved data to reflect any changes.
 	 * 
 	 * @throws HITException if there was an error performing the update.
 	 */
 	public void update() throws HITException {
-		
+
 	}
-	
+
 }
