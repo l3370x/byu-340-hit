13fe7a2f1154d7734b7d382782d4e980cfcb61d3
keith-mcqueen <kemcqueen@mac.com>
2013-07-17 20:34:11 -0600

    Fixing checkstyle (and other) errors.

--- inventory-tracker/test-src/core/model/ItemLabelControllerTest.java
+++ inventory-tracker/test-src/core/model/ItemLabelControllerTest.java
@@ -7,7 +7,6 @@ import static core.model.Item.Factory.newItem;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.After;
