d1968b0ebdac3cb19a43464684eccbb218c43108
keith-mcqueen <kemcqueen@mac.com>
2013-07-18 12:17:52 -0600

    Fixed StackOverflow problem in ItemCollection.

--- inventory-tracker/src/core/model/ItemCollection.java
+++ inventory-tracker/src/core/model/ItemCollection.java
@@ -1,6 +1,7 @@
 package core.model;
 
 import core.model.exception.HITException;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -63,6 +64,16 @@ public class ItemCollection extends AbstractProductContainerProxy<Item> {
     @Override
     public Comparator<Item> getComparator() {
         return null;
+    }
+
+    @Override
+    public Iterable<Item> getItems(Product product) {
+        Set<Item> items = this.itemsByProduct.get(product);
+        if (null == items) {
+            return Collections.emptySet();
+        }
+        
+        return Collections.unmodifiableSet(items);
     }
 
     @Override
