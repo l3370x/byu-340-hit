3403d2b10495e56ca848a736ecda6f7a0b83c903
Cameron Jones <cejones90@msn.com>
2013-07-20 11:13:53 -0600

    fixing item editing

--- inventory-tracker/src/core/model/ItemImpl.java
+++ inventory-tracker/src/core/model/ItemImpl.java
@@ -1,13 +1,13 @@
 package core.model;
 
-import java.util.Date;
-
 import java.util.*;
+
+import common.util.DateUtils;
 
 /**
  * The {@code ItemImpl} class is the default implementation of the {@link Item}
- * interface.  The constructor(s(s) are (mostly) hidden.  To get an Item 
- * instance you must use the {@link Item.Factory}.
+ * interface. The constructor(s(s) are (mostly) hidden. To get an Item instance
+ * you must use the {@link Item.Factory}.
  * 
  * @invariant ?
  * 
@@ -19,45 +19,56 @@ class ItemImpl extends AbstractContainable<ProductContainer> implements Item {
 	private Date exitDate;
 	private Date expirationDate;
 	private Product product;
-	
-    ItemImpl(Date entryDate, Date expirationDate, Product product, BarCode barcode) {
-    	this.product = product;
-    	this.expirationDate = expirationDate;
-    	this.entryDate = entryDate;
-    	this.barcode = barcode;
-    }
 
-    @Override
-    public Product getProduct() {
-        return this.product;
-    }
-    
-    
-    @Override
-    public BarCode getBarCode() {
-        return this.barcode;
-    }
-    
-    
-    @Override
-    public Date getEntryDate() {
-        return this.entryDate;
-    }
-    
+	ItemImpl(Date entryDate, Date expirationDate, Product product,
+			BarCode barcode) {
+		this.product = product;
+		this.expirationDate = expirationDate;
+		this.entryDate = entryDate;
+		this.barcode = barcode;
+	}
 
-    @Override
-    public Date getExitDate() {
-        return this.exitDate;
-    }
-    
-    @Override
-    public void setExitDate(Date d) {
-    	this.exitDate = d;
-    }
+	@Override
+	public Product getProduct() {
+		return this.product;
+	}
 
-    @Override
-    public Date getExpirationDate() {
-        return this.expirationDate;
-    }
-    
+	@Override
+	public BarCode getBarCode() {
+		return this.barcode;
+	}
+
+	@Override
+	public Date getEntryDate() {
+		return this.entryDate;
+	}
+
+	@Override
+	public Date getExitDate() {
+		return this.exitDate;
+	}
+
+	@Override
+	public void setExitDate(Date d) {
+		this.exitDate = d;
+	}
+
+	@Override
+	public Date getExpirationDate() {
+		return this.expirationDate;
+	}
+
+	@Override
+	public void setEntryDate(Date date) {
+		// make checks to date
+		if (date.after(DateUtils.earliestDate())
+				&& date.before(DateUtils.currentDate())) {
+			// Yes. Continue
+			this.entryDate = date;
+			this.notifyObservers(new ModelNotification(
+					ModelNotification.ChangeType.ITEM_UPDATED, this
+							.getContainer(), this));
+		}
+	}
+
 }
--- inventory-tracker/src/gui/item/EditItemController.java
+++ inventory-tracker/src/gui/item/EditItemController.java
@@ -1,18 +1,29 @@
 package gui.item;
 
+import java.util.Date;
+
+import common.util.DateUtils;
+
+import core.model.InventoryManager;
+import static core.model.InventoryManager.Factory.getInventoryManager;
+import core.model.Item;
 import gui.common.*;
 
 /**
  * Controller class for the edit item view.
  */
-public class EditItemController extends Controller 
-										implements IEditItemController {
-	
+public class EditItemController extends Controller implements
+		IEditItemController {
+
+	ItemData item;
+
 	/**
 	 * Constructor.
 	 * 
-	 * @param view Reference to edit item view
-	 * @param target Item that is being edited
+	 * @param view
+	 *            Reference to edit item view
+	 * @param target
+	 *            Item that is being edited
 	 */
 	public EditItemController(IView view, ItemData target) {
 		super(view);
@@ -23,7 +34,7 @@ public class EditItemController extends Controller
 	//
 	// Controller overrides
 	//
-	
+
 	/**
 	 * Returns a reference to the view for this controller.
 	 * 
@@ -33,32 +44,42 @@ public class EditItemController extends Controller
 	 */
 	@Override
 	protected IEditItemView getView() {
-		return (IEditItemView)super.getView();
+		return (IEditItemView) super.getView();
 	}
 
 	/**
 	 * Sets the enable/disable state of all components in the controller's view.
-	 * A component should be enabled only if the user is currently
-	 * allowed to interact with that component.
+	 * A component should be enabled only if the user is currently allowed to
+	 * interact with that component.
 	 * 
 	 * {@pre None}
 	 * 
-	 * {@post The enable/disable state of all components in the controller's view
-	 * have been set appropriately.}
+	 * {@post The enable/disable state of all components in the controller's
+	 * view have been set appropriately.}
 	 */
 	@Override
 	protected void enableComponents() {
+		IEditItemView view = getView();
+		view.enableDescription(false);
+		view.enableBarcode(false);
+		view.enableEntryDate(true);
+		view.enableOK(true);
 	}
 
 	/**
 	 * Loads data into the controller's view.
 	 * 
-	 *  {@pre None}
-	 *  
-	 *  {@post The controller has loaded data into its view}
+	 * {@pre None}
+	 * 
+	 * {@post The controller has loaded data into its view}
 	 */
 	@Override
 	protected void loadValues() {
+		IEditItemView view = getView();
+		Item ourItem = (Item) item.getTag();
+		view.setDescription(ourItem.getProduct().getDescription());
+		view.setBarcode(ourItem.getBarCode().getValue());
+		view.setEntryDate(ourItem.getEntryDate());
 	}
 
 	//
@@ -66,20 +87,27 @@ public class EditItemController extends Controller
 	//
 
 	/**
-	 * This method is called when any of the fields in the
-	 * edit item view is changed by the user.
+	 * This method is called when any of the fields in the edit item view is
+	 * changed by the user.
 	 */
 	@Override
 	public void valuesChanged() {
+		IEditItemView view = getView();
+		Date viewDate = view.getEntryDate();
+		if (viewDate.before(DateUtils.earliestDate())
+				|| viewDate.after(DateUtils.currentDate())) {
+			view.enableOK(false);
+		} else
+			view.enableOK(true);
 	}
-	
+
 	/**
-	 * This method is called when the user clicks the "OK"
-	 * button in the edit item view.
+	 * This method is called when the user clicks the "OK" button in the edit
+	 * item view.
 	 */
 	@Override
 	public void editItem() {
+
 	}
 
 }
-
