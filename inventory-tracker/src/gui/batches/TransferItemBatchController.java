af74a269f0077bac9e24e4574f6a2f5cb80a2a19
Rohit <rohit.iitbbs@gmail.com>
2013-07-22 13:40:37 -0600

    Completed TransferItemBatchController.java

--- inventory-tracker/src/gui/batches/TransferItemBatchController.java
+++ inventory-tracker/src/gui/batches/TransferItemBatchController.java
@@ -1,7 +1,27 @@
 package gui.batches;
 
+import static core.model.InventoryManager.Factory.getInventoryManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.swing.Timer;
+
+import core.model.BarCode;
+import core.model.Item;
+import core.model.Product;
+import core.model.ProductContainer;
+import core.model.StorageUnit;
+import core.model.exception.HITException;
+import core.model.exception.HITException.Severity;
 import gui.common.*;
 import gui.inventory.*;
+import gui.item.ItemData;
 import gui.product.*;
 
 /**
@@ -9,6 +29,12 @@ import gui.product.*;
  */
 public class TransferItemBatchController extends Controller implements
 		ITransferItemBatchController {
+	
+	  private ProductContainerData source;
+	  private static final int TIMER_DELAY = 1000;
+	  private Timer timer;
+	  private final CopyOnWriteArrayList<Product> transferedProducts = new CopyOnWriteArrayList<>();
+	  private final Map<Product, List<Item>> transferedItemsByProduct = new HashMap<>();
 	
 	/**
 	 * Constructor.
@@ -18,7 +44,7 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	public TransferItemBatchController(IView view, ProductContainerData target) {
 		super(view);
-
+		source = target;
 		construct();
 	}
 	
@@ -39,6 +65,18 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	@Override
 	protected void loadValues() {
+        // default to use the scanner
+        this.getView().setUseScanner(true);
+        this.useScannerChanged();
+
+        // give focus to the barcode field
+        this.getView().giveBarcodeFocus();
+        
+        // if using a scanner, clear the barcode field
+        if (this.getView().getUseScanner()) {
+            this.getView().setBarcode("");
+        }
+		
 	}
 
 	/**
@@ -53,6 +91,9 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	@Override
 	protected void enableComponents() {
+		  this.getView().enableItemAction(false);
+	        this.getView().enableUndo(false);
+	        this.getView().enableRedo(false);
 	}
 
 	/**
@@ -61,7 +102,42 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	@Override
 	public void barcodeChanged() {
+		  if (false == this.getView().getUseScanner()) {
+	            this.ensureItemExists();
+	            return;
+	        }
+
+	        String barcode = this.getView().getBarcode();
+	        if (null == barcode || barcode.isEmpty()) {
+	            return;
+	        }
+
+	        if (this.timer.isRunning()) {
+	            this.timer.restart();
+	        } else {
+	            this.timer.start();
+	        }
 	}
+	
+	  private boolean ensureItemExists() {
+	        final BarCode barcode = BarCode.getBarCodeFor(this.getView().getBarcode());
+	        if (barcode.toString().length() != 12) {
+	            enableComponents();
+	            return false;
+
+	        }
+
+	        // see if the Item exists in the InventoryManager
+	        Item item = getInventoryManager().getItem(barcode);
+
+	        if (item == null) {
+	            enableComponents();
+	            return false;
+	        } else {
+	            this.getView().enableItemAction(true);
+	            return true;
+	        }
+	    }
 	
 	/**
 	 * This method is called when the "Use Barcode Scanner" setting in the
@@ -69,7 +145,26 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	@Override
 	public void useScannerChanged() {
+		  if (this.getView().getUseScanner()) {
+	            this.timer.stop();
+	            this.getView().enableItemAction(false);
+	        } else {
+	            this.initTimer();
+	        }
 	}
+	
+	
+	 private void initTimer() {
+	        if (null == this.timer) {
+	            this.timer = new Timer(TIMER_DELAY, new ActionListener() {
+	                @Override
+	                public void actionPerformed(ActionEvent e) {
+	                    transferItem();
+	                }
+	            });
+	            this.timer.setRepeats(false);
+	        }
+	    }
 	
 	/**
 	 * This method is called when the selected product changes
@@ -77,6 +172,29 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	@Override
 	public void selectedProductChanged() {
+		
+
+        ProductData productData = this.getView().getSelectedProduct();
+        if (null == productData) {
+            return;
+        }
+
+        Object tag = productData.getTag();
+        if (false == tag instanceof Product) {
+            return;
+        }
+
+        List<Item> removedItems = this.transferedItemsByProduct.get((Product) tag);
+        if (null == removedItems) {
+            return;
+        }
+
+        List<ItemData> itemList = new ArrayList<>();
+        for (Item item : removedItems) {
+            itemList.add(new ItemData(item));
+        }
+        this.getView().setItems(itemList.toArray(new ItemData[itemList.size()]));
+
 	}
 	
 	/**
@@ -85,7 +203,73 @@ public class TransferItemBatchController extends Controller implements
 	 */
 	@Override
 	public void transferItem() {
+		if (this.ensureItemExists() == false) {
+			return;
+	    }
+		   
+		final BarCode barcode = BarCode.getBarCodeFor(this.getView().getBarcode());
+	    Item item = getInventoryManager().getItem(barcode);
+	    
+	    try {
+	    	
+            Object tag = this.source.getTag();
+            if (false == tag instanceof StorageUnit) {
+                throw new HITException(Severity.ERROR, 
+                        "Items cannot be transfered without a target Storage Unit");
+            }
+            ProductContainer targetUnit = (StorageUnit) tag;
+                       
+           item.transfer(item.getContainer(), targetUnit);
+            
+            
+            //Update Views
+           this.updateProductsPane(item);
+            this.loadValues();
+            this.enableComponents();
+
+        } catch (HITException e) {
+            this.getView().displayErrorMessage("Could Not Remove Item: " + e.getMessage());
+        }
 	}
+	
+	
+	 private void updateProductsPane(Item item) {
+	        Product product = item.getProduct();
+	        // add the product to the list if it hasn't been already
+	        this.transferedProducts.addIfAbsent(product);
+	        if (this.transferedItemsByProduct.containsKey(product)) {
+	            List<Item> list = this.transferedItemsByProduct.get(product);
+	            list.add(item);
+	            this.transferedItemsByProduct.put(product, list);
+	        } else {
+	            List<Item> list = new ArrayList<>();
+	            list.add(item);
+	            this.transferedItemsByProduct.put(product, list);
+	        }
+
+	        // create the product data instances
+	        ProductData selected = null;
+	        List<ProductData> productList = new ArrayList<>();
+	        for (Product p : this.transferedProducts) {
+	            ProductData data = new ProductData(p);
+	            data.setCount(Integer.toString(this.transferedItemsByProduct.get(p).size()));
+	            productList.add(data);
+	            if (p == item.getProduct()) {
+	                selected = data;
+	            }
+	        }
+
+	        // display the products in the view
+	        this.getView().setProducts(productList.toArray(new ProductData[productList.size()]));
+
+	        // select the just-added product
+	        if (null != selected) {
+	            this.getView().selectProduct(selected);
+	            this.selectedProductChanged();
+	        }
+	    }
+
+	  
 	
 	/**
 	 * This method is called when the user clicks the "Redo" button
