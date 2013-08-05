1df3237deca9ff3b35f9b377c95323516d98a2cd
Cameron Jones <cejones90@msn.com>
2013-08-05 13:54:46 -0600

    changes to Semantics plugin

--- inventory-tracker/src/gui/product/UPCDatabaseSemantics.java
+++ inventory-tracker/src/gui/product/UPCDatabaseSemantics.java
@@ -1,8 +1,23 @@
 package gui.product;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.Iterator;
+
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.semantics3.*;
+import com.semantics3.api.Products;
 
 public class UPCDatabaseSemantics implements ProductDetector {
 	
@@ -13,14 +28,22 @@ public class UPCDatabaseSemantics implements ProductDetector {
 
 	@Override
 	public ProductDescriptor getProductDescription(String barcode) {
+		Products products = new Products(API_KEY, API_SECRET);
+		
+		products.productsField("upc", barcode);
+		
 		try {
-			URL url = new URL(MessageFormat.format(URL_FORMAT, BARCODE_ELEMENT + barcode));
-			
-		} catch (MalformedURLException e) {
+			JSONObject object = products.getProducts();
+			JSONTokener token = new JSONTokener(object.toString());
+			JSONArray results = new JSONArray(token);
+			System.out.println(results);
+		} catch (OAuthMessageSignerException | OAuthExpectationFailedException
+				| OAuthCommunicationException | IOException | JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
+		
 	}
 
 }
