c9426a721d0c070f749db0bc83bc252964bec9a4
keith-mcqueen <kemcqueen@mac.com>
2013-08-06 08:31:16 -0600

    More minor fixes.

--- inventory-tracker/src/persistence/ProductDAO.java
+++ inventory-tracker/src/persistence/ProductDAO.java
@@ -1,11 +1,10 @@
 /**
- * 
+ *
  */
 package persistence;
 
 /**
  * @author rfun11
- *
  */
 public class ProductDAO extends AbstractDAO {
 
@@ -19,26 +18,31 @@ public class ProductDAO extends AbstractDAO {
     public static final String COL_3_MONTH_SUPPLY = "_3_MONTH_SUPPLY";
 
     private static final String[] COLUMN_NAMES = new String[]{
-        COL_BARCODE,
-        COL_DESCRIPTION,
-        COL_SIZE_AMT,
-        COL_SIZE_UNIT,
-        COL_SHELF_LIFE_MONTHS,
-        COL_CREATE_DATE,
-        COL_3_MONTH_SUPPLY
-};
-    
-    
-	@Override
-	public String[] getColumnNames() {
-		 return COLUMN_NAMES;
-	}
+            COL_BARCODE,
+            COL_DESCRIPTION,
+            COL_SIZE_AMT,
+            COL_SIZE_UNIT,
+            COL_SHELF_LIFE_MONTHS,
+            COL_CREATE_DATE,
+            COL_3_MONTH_SUPPLY
+    };
 
-	@Override
-	public String getTableName() {
-		 return TABLE_NAME;
-	}
-	
-	
+    private static final String[] KEY_COLUMNS = new String[]{COL_BARCODE};
+
+    @Override
+    public String[] getColumnNames() {
+        return COLUMN_NAMES;
+    }
+
+    @Override
+    public String[] getKeyColumnNames() {
+        return KEY_COLUMNS;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
 
 }
