02257f0115a7e1f45e62034c81369af5932f1b1d
Aaron Miller <l3370x@gmail.com>
2013-08-06 13:22:32 -0600

    fixed setting/getting last removed date

--- inventory-tracker/src/persistence/RemovedItemsDAO.java
+++ inventory-tracker/src/persistence/RemovedItemsDAO.java
@@ -2,25 +2,26 @@ package persistence;
 
 public class RemovedItemsDAO extends AbstractDAO {
 
-    public static final String TABLE_NAME = "REMOVED_ITEMS_REPORT";
-    public static final String COL_LAST_REPORT_RUN = "LAST_REPORT_RUN";
+	public static final String TABLE_NAME = "REMOVED_ITEMS_REPORT";
+	public static final String COL_ID = "ID";
+	public static final String COL_LAST_REPORT_RUN = "LAST_REPORT_RUN";
 
-    private static final String[] COLUMN_NAMES = new String[]{
-            COL_LAST_REPORT_RUN
-    };
+	private static final String[] COLUMN_NAMES = new String[] { COL_ID, COL_LAST_REPORT_RUN };
 
-    @Override
-    public String[] getColumnNames() {
-        return COLUMN_NAMES;
-    }
+	private static final String[] COLUMN_KEY_NAMES = new String[] { COL_ID };
 
-    @Override
-    public String[] getKeyColumnNames() {
-        return COLUMN_NAMES;
-    }
+	@Override
+	public String[] getColumnNames() {
+		return COLUMN_NAMES;
+	}
 
-    @Override
-    public String getTableName() {
-        return TABLE_NAME;
-    }
+	@Override
+	public String[] getKeyColumnNames() {
+		return COLUMN_KEY_NAMES;
+	}
+
+	@Override
+	public String getTableName() {
+		return TABLE_NAME;
+	}
 }
