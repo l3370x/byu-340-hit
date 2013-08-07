59339ecb33465cba8e0d92fadcc9cfe545813b78
Rohit <rohit.iitbbs@gmail.com>
2013-08-06 20:24:45 -0600

    Slightly modified the ProductStats Supply tests

--- inventory-tracker/test-src/gui/reports/productstats/ProductStatsMaximumSupplyTests.java
+++ inventory-tracker/test-src/gui/reports/productstats/ProductStatsMaximumSupplyTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Calendar;
 
 import static gui.reports.productstats.ProductStatsCalculator.Factory.newProductStatsCalculator;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Cameron Jones (originator)
@@ -13,39 +14,39 @@ import static gui.reports.productstats.ProductStatsCalculator.Factory.newProduct
 public class ProductStatsMaximumSupplyTests extends ProductStatsCalculatorTests {
     @Test
     public void testCalculateMaximumSupply1Month() {
-        this.testCalculateMaxSupply(1);
+        this.testCalculateMaxSupply(1, 6);
     }
 
     @Test
     public void testCalculateMaximumSupply3Month() {
-        this.testCalculateMaxSupply(3);
+        this.testCalculateMaxSupply(3,6 );
     }
 
     @Test
     public void testCalculateMaximumSupply6Month() {
-        this.testCalculateMaxSupply(6);
+        this.testCalculateMaxSupply(6,6);
     }
 
     @Test
     public void testCalculateMaximumSupply9Month() {
-        this.testCalculateMaxSupply(9);
+        this.testCalculateMaxSupply(9,6);
     }
 
     @Test
     public void testCalculateMaximumSupply12Month() {
-        this.testCalculateMaxSupply(12);
+        this.testCalculateMaxSupply(12,6);
     }
 
     @Test
     public void testCalculateMaximumSupply24Month() {
-        this.testCalculateMaxSupply(24);
+        this.testCalculateMaxSupply(24,6);
     }
 
-    private void testCalculateMaxSupply(int months) {
+    private void testCalculateMaxSupply(int months, int expected) {
         cal.add(Calendar.MONTH, -months);
         calc = newProductStatsCalculator(cal.getTime(), list.returnCurrent(),
                 list.returnRemoved());
-        out.println("Maximum Supply: " + months + " month = "
-                + calc.maximumSupply());
+        int x = calc.maximumSupply();
+        assertEquals(x,expected);
     }
 }
--- inventory-tracker/test-src/gui/reports/productstats/ProductStatsMinimumSupplyTests.java
+++ inventory-tracker/test-src/gui/reports/productstats/ProductStatsMinimumSupplyTests.java
@@ -2,50 +2,70 @@ package gui.reports.productstats;
 
 import org.junit.Test;
 
+import core.model.ItemCollection;
+
 import java.util.Calendar;
 
 import static gui.reports.productstats.ProductStatsCalculator.Factory.newProductStatsCalculator;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Cameron Jones (originator)
  * @author Keith McQueen (refactoring)
+ * @author Rohit
  */
 public class ProductStatsMinimumSupplyTests extends ProductStatsCalculatorTests {
     @Test
     public void testCalculateMinimumSupply1Month() {
-        this.testCalculateMinimumSupply(1);
+        this.testCalculateMinimumSupply(1, 3);
     }
 
     @Test
     public void testCalculateMinimumSupply3Month() {
-        this.testCalculateMinimumSupply(3);
+        this.testCalculateMinimumSupply(3, 2);
     }
 
     @Test
     public void testCalculateMinimumSupply6Month() {
-        this.testCalculateMinimumSupply(6);
+        this.testCalculateMinimumSupply(6, 2);
     }
 
     @Test
     public void testCalculateMinimumSupply9Month() {
-        this.testCalculateMinimumSupply(9);
+        this.testCalculateMinimumSupply(9, 2);
     }
 
     @Test
     public void testCalculateMinimumSupply12Month() {
-        this.testCalculateMinimumSupply(12);
+        this.testCalculateMinimumSupply(12, 2);
     }
 
     @Test
     public void testCalculateMinimumSupply24Month() {
-        this.testCalculateMinimumSupply(24);
+        this.testCalculateMinimumSupply(24, 0);
     }
+    
+    @Test
+    public void testCalculateMinimumSupply3MonthWithZeroItems() {
+    	ItemCollection currentItems = new ItemCollection(null);
+    	
+    	cal.add(Calendar.MONTH, -3);
+        calc = newProductStatsCalculator(cal.getTime(), currentItems.getContents(),
+                list.returnRemoved());
+        
+        assertEquals(calc.minimumSupply(),0);
+    	
+    }
+    
 
-    private void testCalculateMinimumSupply(int months) {
+    private void testCalculateMinimumSupply(int months, int expected) {
         cal.add(Calendar.MONTH, -months);
         calc = newProductStatsCalculator(cal.getTime(), list.returnCurrent(),
                 list.returnRemoved());
-        out.println("Minimum Supply: " + months + " month = "
-                + calc.minimumSupply());
+        int x = calc.minimumSupply();
+        assertEquals(x,expected);
+    	
+       // out.println("Minimum Supply: " + months + " month = "
+              //  + calc.minimumSupply());
     }
 }
